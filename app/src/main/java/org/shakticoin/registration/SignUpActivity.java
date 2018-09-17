7641c95e0f88f064b40bb4d8feaaad585f87e1a1
Oleg Andreyev <andreyev.oleg@gmail.com>
2018-09-17 20:10:54 +0300

    Initial project files

---
+++ app/src/main/java/org/shakticoin/registration/SignUpActivity.java
@@ -0,0 +1,17 @@
+package org.shakticoin.registration;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import org.shakticoin.R;
+
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
