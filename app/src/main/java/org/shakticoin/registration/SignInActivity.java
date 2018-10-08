38433d02ae4076758ac7e3c8256f13c464c5c3fa
Oleg Andreyev <andreyev.oleg@gmail.com>
2018-10-08 19:41:51 +0300

    Add progress indicator to the login screen

--- app/src/main/java/org/shakticoin/registration/SignInActivity.java
+++ app/src/main/java/org/shakticoin/registration/SignInActivity.java
@@ -1,5 +1,6 @@
 package org.shakticoin.registration;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,10 +9,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,6 +35,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class SignInActivity extends AppCompatActivity {
     private EditText ctrlUsername;
     private EditText ctrlPassword;
+    private ProgressBar progressBar;
 
     private LoginService loginService;
 
@@ -42,34 +44,26 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        progressBar = findViewById(R.id.progressBar);
         ctrlUsername = findViewById(R.id.username);
         ctrlPassword = findViewById(R.id.password);
-        ctrlPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    onLogin(v);
-                    return true;
-                }
-                return false;
+        ctrlPassword.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                onLogin(v);
+                return true;
             }
+            return false;
         });
         TextView ctrlRegisterNowLink = findViewById(R.id.register_now_link);
         final Context self = this;
-        ctrlRegisterNowLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(self, SignUpActivity.class);
-                startActivity(intent);
-            }
+        ctrlRegisterNowLink.setOnClickListener(v -> {
+            Intent intent = new Intent(self, SignUpActivity.class);
+            startActivity(intent);
         });
         TextView ctrlForgotPwd = findViewById(R.id.forgotPassword);
-        ctrlForgotPwd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(self, RecoveryPasswordActivity.class);
-                startActivity(intent);
-            }
+        ctrlForgotPwd.setOnClickListener(v -> {
+            Intent intent = new Intent(self, RecoveryPasswordActivity.class);
+            startActivity(intent);
         });
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -89,10 +83,13 @@ public class SignInActivity extends AppCompatActivity {
             return;
         }
 
+        progressBar.setVisibility(View.VISIBLE);
+
         Credentials credentials = new Credentials();
         credentials.setUsername(username);
         credentials.setPassword(password);
 
+        final Activity self = this;
         Call<LoginServiceResponse> call = loginService.login(credentials);
         call.enqueue(new Callback<LoginServiceResponse>() {
             @Override
@@ -105,10 +102,13 @@ public class SignInActivity extends AppCompatActivity {
                         prefs.edit().putBoolean(PreferenceHelper.PREF_KEY_HAS_ACCOUNT, true).apply();
                     }
                 }
+                progressBar.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onFailure(@NonNull Call<LoginServiceResponse> call, @NonNull Throwable t) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(self, R.string.err_unexpected, Toast.LENGTH_SHORT).show();
                 Debug.logException(t);
             }
         });
