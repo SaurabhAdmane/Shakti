db18dd7977943d35481a51df6acb2d6a464de0a5
Oleg Andreyev <andreyev.oleg@gmail.com>
2018-10-16 17:16:14 +0300

    Recent changes in Log In screen

---
+++ app/src/main/java/org/shakticoin/registration/ReferralConfirmationActivity.java
@@ -0,0 +1,21 @@
+package org.shakticoin.registration;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import org.shakticoin.R;
+
+
+public class ReferralConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referral_confirmation);
+    }
+
+    public void onGoWallet(View view) {
+    }
+}
