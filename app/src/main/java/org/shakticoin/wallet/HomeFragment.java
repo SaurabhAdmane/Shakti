bdb15bc8b6a16d74411cae45b19f60eb833caaac
Oleg Andreyev <andreyev.oleg@gmail.com>
2018-10-28 17:12:06 +0200

    Add Share functionality to corresponding buttons in wallet

--- app/src/main/java/org/shakticoin/wallet/HomeFragment.java
+++ app/src/main/java/org/shakticoin/wallet/HomeFragment.java
@@ -1,15 +1,18 @@
 package org.shakticoin.wallet;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.shakticoin.R;
 import org.shakticoin.databinding.FragmentWalletHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -33,6 +36,31 @@ public class HomeFragment extends Fragment {
         binding.setLifecycleOwner(this);
         if (viewModel != null) binding.setViewModel(viewModel);
 
+        binding.onShareCode.setOnClickListener(v -> shareReferralCode());
+        binding.onShareLink.setOnClickListener(v -> shareReferralUrl());
+
         return binding.getRoot();
+    }
+
+    private void shareReferralUrl() {
+        String referralUrl = viewModel.referralLink.get();
+        if (!TextUtils.isEmpty(referralUrl)) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, referralUrl);
+            startActivity(Intent.createChooser(intent, getString(R.string.share_referral_link)));
+        }
+    }
+
+    private void shareReferralCode() {
+        String referralCode = viewModel.referralCode.get();
+        if (!TextUtils.isEmpty(referralCode)) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, referralCode);
+            startActivity(Intent.createChooser(intent, getString(R.string.share_referral_code)));
+        }
     }
 }
