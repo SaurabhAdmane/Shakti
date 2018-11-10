af00c9389ad922b571b856b0774f33341b5a8089
Oleg Andreyev <andreyev.oleg@gmail.com>
2018-11-10 22:08:27 +0200

    Apply new widget to sign in activity

--- app/src/main/java/org/shakticoin/registration/SignInActivity.java
+++ app/src/main/java/org/shakticoin/registration/SignInActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,7 +13,6 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.shakticoin.BuildConfig;
@@ -32,6 +30,7 @@ import org.shakticoin.util.Debug;
 import org.shakticoin.util.PreferenceHelper;
 import org.shakticoin.util.Validator;
 import org.shakticoin.wallet.WalletActivity;
+import org.shakticoin.widget.TextInputLayout;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -43,10 +42,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class SignInActivity extends AppCompatActivity {
     private EditText ctrlUsername;
     private EditText ctrlPassword;
+    private TextInputLayout ctrlPasswordLayout;
     private ProgressBar progressBar;
 
     private LoginService loginService;
     private MinerRepository minerRepository;
+
+    private static final int MIN_PASSWD_LEN = 8;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,6 +64,17 @@ public class SignInActivity extends AppCompatActivity {
                 return true;
             }
             return false;
+        });
+
+        TextInputLayout ctrlUsernameLayout = findViewById(R.id.username_layout);
+        ctrlUsernameLayout.setValidator((view, value) -> {
+            // username must be email
+            return Validator.isEmail(value);
+        });
+        ctrlPasswordLayout = findViewById(R.id.password_layout);
+        ctrlPasswordLayout.setValidator((view, value) -> {
+            // password must be longer than 8 chars
+            return value != null && value.length() > MIN_PASSWD_LEN;
         });
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -141,7 +154,10 @@ public class SignInActivity extends AppCompatActivity {
                         }
                     } else {
                         progressBar.setVisibility(View.INVISIBLE);
-                        Toast.makeText(self, R.string.err_login_failed, Toast.LENGTH_LONG).show();
+                        // TODO: for demonstration purpose. The callout positioned on password field
+                        // but we don't know (and shoudl not to say) if it's a password problem
+//                        Toast.makeText(self, R.string.err_login_failed, Toast.LENGTH_LONG).show();
+                        ctrlPasswordLayout.setError(getString(R.string.err_login_failed));
                     }
                 }
             }
@@ -153,6 +169,7 @@ public class SignInActivity extends AppCompatActivity {
                         BuildConfig.DEBUG ? t.getMessage() : getString(R.string.err_unexpected),
                         Toast.LENGTH_SHORT).show();
                 Debug.logException(t);
+                ctrlPasswordLayout.setError(getString(R.string.err_login_failed));
             }
         });
     }
