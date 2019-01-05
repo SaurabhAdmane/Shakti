cb508223796fc0ed5f9f51d0636f7a2863e0515e
Oleg Andreyev <andreyev.oleg@gmail.com>
2019-01-05 21:26:46 +0200

    Revert debugging code

--- app/src/main/java/org/shakticoin/SplashActivity.java
+++ app/src/main/java/org/shakticoin/SplashActivity.java
@@ -21,8 +21,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_splash);
 
-//        route();
-        startActivity(new Intent(this, BonusBountyActivity.class));
+        route();
         finish();
     }
 
