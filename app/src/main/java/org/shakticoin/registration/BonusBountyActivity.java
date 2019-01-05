9db84dd5d5567534b1a10fa0d1612b40c5774c8c
Oleg Andreyev <andreyev.oleg@gmail.com>
2019-01-05 21:26:24 +0200

    Implement internal logic for bounty bonus screen

--- app/src/main/java/org/shakticoin/registration/BonusBountyActivity.java
+++ app/src/main/java/org/shakticoin/registration/BonusBountyActivity.java
@@ -1,10 +1,7 @@
 package org.shakticoin.registration;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
-import android.databinding.InverseBindingAdapter;
-import android.databinding.InverseBindingListener;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +10,6 @@ import android.widget.Toast;
 
 import org.shakticoin.R;
 import org.shakticoin.databinding.ActivityBountyBinding;
-import org.shakticoin.widget.CheckableRoundButton;
 
 public class BonusBountyActivity extends AppCompatActivity {
 
@@ -23,24 +19,11 @@ public class BonusBountyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_bounty);setContentView(R.layout.activity_bounty);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_bounty);
+        binding.setLifecycleOwner(this);
 
         viewModel = ViewModelProviders.of(this).get(BonusBountyModel.class);
-    }
-
-    @BindingAdapter("is_checked")
-    public static void setButtonState(CheckableRoundButton view, Boolean value) {
-        view.setChecked(value);
-    }
-
-    @InverseBindingAdapter(attribute = "is_checked", event = "app:is_checkedAttrChanged")
-    public static Boolean getButtonState(CheckableRoundButton view) {
-        return view.isChecked();
-    }
-
-    @BindingAdapter("app:is_checkedAttrChanged")
-    public static void setListeners(CheckableRoundButton view, final InverseBindingListener attrChange) {
-        view.setOnCheckedChangeListener((buttonView, isChecked) -> attrChange.onChange());
+        binding.setViewModel(viewModel);
     }
 
     public void onClaim(View v) {
