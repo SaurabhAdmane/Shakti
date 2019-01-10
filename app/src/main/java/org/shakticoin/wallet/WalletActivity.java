fdd444fd6c7cfe0697a503a20ba80694cd1659d5
Oleg Andreyev <andreyev.oleg@gmail.com>
2019-01-10 20:13:50 +0200

    Add menu and notification drawers

--- app/src/main/java/org/shakticoin/wallet/WalletActivity.java
+++ app/src/main/java/org/shakticoin/wallet/WalletActivity.java
@@ -1,19 +1,36 @@
 package org.shakticoin.wallet;
 
+import android.animation.ValueAnimator;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.shakticoin.R;
 import org.shakticoin.databinding.ActivityWalletBinding;
 import org.shakticoin.registration.SignInActivity;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
 public class WalletActivity extends AppCompatActivity {
     private ActivityWalletBinding binding;
     private WalletModel viewModel;
+
+    private RecyclerView notifications;
+    private NotificationAdapter notificationsAdapter;
 
     @Override
     public void onBackPressed() {
@@ -33,9 +50,144 @@ public class WalletActivity extends AppCompatActivity {
 
         viewModel = ViewModelProviders.of(this).get(WalletModel.class);
 
+        // setup list of notifications
+        notifications = findViewById(R.id.notificationList);
+        notifications.setHasFixedSize(false);
+        notifications.setLayoutManager(new LinearLayoutManager(this));
+        notificationsAdapter = new NotificationAdapter();
+        notifications.setAdapter(notificationsAdapter);
+
+        viewModel.notifications.observe(this, notifications -> notificationsAdapter.notifyDataSetChanged());
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.fragments, new MainFragment(), MainFragment.class.getSimpleName())
+                .replace(R.id.mainFragment, new MainFragment(), MainFragment.class.getSimpleName())
                 .commit();
+    }
+
+    public void onDrawer(View v) {
+        View drawer = findViewById(R.id.drawerContainer);
+        if (drawer != null) {
+            int openedWidth = getResources().getDimensionPixelSize(R.dimen.drawer_width);
+            int currentWidth = drawer.getMeasuredWidth();
+            // we need close the notification messages before open drawer because main content will
+            // be shifted over it
+            if (currentWidth == 0) {
+                View notificationsContainer = findViewById(R.id.notificationContainer);
+                float currentOffset = notificationsContainer.getTranslationX();
+                if (Math.abs(currentOffset) < 0.1) {
+                    int containerWidth = getResources().getDimensionPixelOffset(R.dimen.notifications_list_width);
+                    notificationsContainer.setTranslationX(containerWidth);
+                }
+            }
+            ValueAnimator anim = ValueAnimator.ofInt(currentWidth, currentWidth == 0 ? openedWidth : 0);
+            anim.addUpdateListener(animation -> {
+                int newWidth = (int) animation.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = drawer.getLayoutParams();
+                layoutParams.width = newWidth;
+                drawer.setLayoutParams(layoutParams);
+                // move main fragment to the same direction
+                binding.mainFragment.setTranslationX(newWidth);
+            });
+            anim.setDuration(250);
+            anim.start();
+        }
+    }
+
+    public void onNotifications(View v) {
+        View notificationsContainer = findViewById(R.id.notificationContainer);
+        if (notificationsContainer != null) {
+            int openedWidth = getResources().getDimensionPixelSize(R.dimen.notifications_list_width);
+            int currentWidth = notificationsContainer.getMeasuredWidth();
+            // we need close the drawer before open notification messages because main content will
+            // be shifted over it
+            if (Math.abs(notificationsContainer.getTranslationX() - openedWidth) < 0.1) {
+                View drawer = findViewById(R.id.drawerContainer);
+                ViewGroup.LayoutParams layoutParams = drawer.getLayoutParams();
+                layoutParams.width = 0;
+                drawer.setLayoutParams(layoutParams);
+            }
+
+            int startPosition = notificationsContainer.getTranslationX() == 0 ? 0 : currentWidth;
+            int endPosition = notificationsContainer.getTranslationX() == 0 ? currentWidth : 0;
+
+            ValueAnimator anim = ValueAnimator.ofInt(startPosition, endPosition);
+            anim.addUpdateListener(animation -> {
+                int newWidth = (int) animation.getAnimatedValue();
+                notificationsContainer.setTranslationX(newWidth);
+                // move main fragment to the same direction
+                binding.mainFragment.setTranslationX((openedWidth - newWidth) * -1);
+            });
+            anim.setDuration(500);
+            anim.start();
+        }
+    }
+
+    public void onOpenReferrals(View v) {
+        Toast.makeText(this, R.string.err_not_implemented, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onOpenSettings(View v) {
+        Toast.makeText(this, R.string.err_not_implemented, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onOpenMiner(View v) {
+        Toast.makeText(this, R.string.err_not_implemented, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onOpenVault(View v) {
+        Toast.makeText(this, R.string.err_not_implemented, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onOpenWallet(View v) {
+        Toast.makeText(this, R.string.err_not_implemented, Toast.LENGTH_SHORT).show();
+    }
+
+
+    class NotificationViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView message;
+        public TextView date;
+
+        NotificationViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            message = itemView.findViewById(R.id.message);
+            date = itemView.findViewById(R.id.date);
+        }
+    }
+
+    class NotificationAdapter extends RecyclerView.Adapter<NotificationViewHolder> {
+
+        @NonNull
+        @Override
+        public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int type) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false);
+            return new NotificationViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NotificationViewHolder viewHolder, int position) {
+            WalletModel.Notification notification =
+                    Objects.requireNonNull(viewModel.notifications.getValue()).get(position);
+            if (notification != null) {
+                viewHolder.title.setText(notification.getTitle());
+                viewHolder.message.setText(notification.getMessage());
+                Date date = notification.getDate();
+                DateFormat fmt = DateFormat.getDateInstance(DateFormat.SHORT);
+                viewHolder.date.setText(fmt.format(date));
+                if (notification.isRead()) {
+                    viewHolder.title.setTextColor(getResources().getColor(R.color.colorReadMessage));
+                    viewHolder.message.setTextColor(getResources().getColor(R.color.colorReadMessage));
+                    viewHolder.date.setTextColor(getResources().getColor(R.color.colorReadMessage));
+                }
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            List<WalletModel.Notification> list = viewModel.notifications.getValue();
+            return list != null ? list.size() : 0;
+        }
     }
 }
--- app/src/main/java/org/shakticoin/wallet/WalletModel.java
+++ app/src/main/java/org/shakticoin/wallet/WalletModel.java
@@ -1,9 +1,9 @@
 package org.shakticoin.wallet;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
-import android.util.Log;
 
 import org.shakticoin.api.BaseUrl;
 import org.shakticoin.api.OnCompleteListener;
@@ -12,6 +12,10 @@ import org.shakticoin.api.miner.MinerRepository;
 import org.shakticoin.api.miner.User;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class WalletModel extends ViewModel {
@@ -22,6 +26,9 @@ public class WalletModel extends ViewModel {
 
     public ObservableField<BigDecimal> balance = new ObservableField<>(BigDecimal.ZERO);
     public ObservableField<BigDecimal> bonusBalance = new ObservableField<>(BigDecimal.ZERO);
+
+    /** List of unread notification messages */
+    MutableLiveData<List<Notification>> notifications = new MutableLiveData<>();
 
     public WalletModel() {
         progressBarTrigger.set(true);
@@ -56,6 +63,28 @@ public class WalletModel extends ViewModel {
                 }
             }
         });
+
+        /*
+         * TODO: Create a list of notifications for testing purpose. Replace with real stuff later.
+         */
+        List<Notification> testList = new ArrayList<>();
+        Notification notification = new Notification();
+        notification.setTitle("Welcome to your Wallet.");
+        notification.setMessage("Complete your KYC verification to fully unlock it and get ready for your Bonus Bounty!");
+        notification.setDate(new Date(2019, 1, 6));
+        testList.add(notification);
+        notification = new Notification();
+        notification.setTitle("Congratulations!");
+        notification.setMessage("You've choosen the 1008.00 SXE Bonus Bounty. Refer friends to unlock it sooner!");
+        notification.setDate(new Date(2019, 1, 7));
+        testList.add(notification);
+        notification = new Notification();
+        notification.setTitle("A friend has singed up.");
+        notification.setMessage("Alan Reynolds has registered you as a referral upon singing up.");
+        notification.setDate(new Date(2019, 1, 12));
+        testList.add(notification);
+        Collections.sort(testList, (o1, o2) -> o1.getDate().compareTo(o2.getDate()));
+        notifications.setValue(testList);
     }
 
     public String getFormattedBalance() {
@@ -63,5 +92,48 @@ public class WalletModel extends ViewModel {
     }
     public String getFormattedBonusBalance() {
         return String.format(Locale.getDefault(), "SXE %1$.2f", bonusBalance.get());
+    }
+
+    /**
+     * Class that holds a notification message and use as a model for list of notifications
+     * in pull down list.
+     */
+    public class Notification {
+        private String title;
+        private String message;
+        private Date date;
+        private boolean read = false;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public boolean isRead() {
+            return read;
+        }
+
+        public void setRead(boolean read) {
+            this.read = read;
+        }
     }
 }
