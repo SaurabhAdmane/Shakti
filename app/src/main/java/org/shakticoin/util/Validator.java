ccb3f64171522330d84815b5f0473ecf4e772228
Oleg Andreyev <andreyev.oleg@gmail.com>
2019-01-23 22:15:15 +0200

    Stronger validation for the phone number

--- app/src/main/java/org/shakticoin/util/Validator.java
+++ app/src/main/java/org/shakticoin/util/Validator.java
@@ -4,6 +4,9 @@ import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Patterns;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
 
     /**
@@ -14,12 +17,35 @@ public class Validator {
         return emailAddress != null && Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches();
     }
 
+    private static List<Character> allowedCharsPhone = Arrays.asList('.',' ','-','(',')');
+
     /**
      * Returns true if a given String might be a phone number.
      * @see android.util.Patterns
      */
     public static boolean isPhoneNumber(String phoneNumber) {
-        return phoneNumber != null && Patterns.PHONE.matcher(phoneNumber).matches();
+        if (TextUtils.isEmpty(phoneNumber)) return false;
+
+        // + is allowed only as a first char
+        String number = phoneNumber.charAt(0) == '+' ? phoneNumber.substring(1) : phoneNumber;
+        if (number.length() == 0) return false;
+
+        // only digit allowed at first and last position
+        if (!Character.isDigit(number.charAt(0))) return false;
+        if (!Character.isDigit(number.charAt(number.length()-1))) return false;
+
+        int digitsCount = 0;
+        for(int i = 0; i < number.length(); i++){
+            final char c = number.charAt(i);
+            if (Character.isDigit(c)) {
+                digitsCount++;
+            } else {
+                // only -, ., (, ) and space are allowed
+                if (!allowedCharsPhone.contains(c)) return false;
+            }
+        }
+        if (digitsCount < 10) return false;
+        return true;
     }
 
     /**
