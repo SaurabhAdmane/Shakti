192a38b4edc6b5f2f64c5e436ffe623f20f6fd90
Oleg Andreyev <andreyev.oleg@gmail.com>
2019-01-28 16:46:05 +0200

    Add collection of the phone country codes

--- app/src/main/java/org/shakticoin/room/AppDatabase.java
+++ app/src/main/java/org/shakticoin/room/AppDatabase.java
@@ -4,30 +4,55 @@ import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 
 import org.shakticoin.room.entity.Geo;
+import org.shakticoin.room.entity.PhoneCountryCode;
 
 import java.util.concurrent.Executors;
 
 
-@Database(entities = {Geo.class}, version = 1)
+@Database(version = 2, entities = {Geo.class, PhoneCountryCode.class})
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase database;
 
-    public abstract GeoDao geoDao();
+    abstract public PhoneCountryCodeDao phoneCountryCodeDao();
+    abstract public GeoDao geoDao();
 
     public synchronized  static AppDatabase getDatabase(Context context) {
         if (database == null) {
-            database = buildDatabase(context);
+            synchronized (AppDatabase.class) {
+                if (database == null) {
+                    database = buildDatabase(context);
+                }
+            }
         }
         return database;
     }
 
+    /**
+     * One new table PhoneCountryCode and we need to populate it with seed data.
+     */
+    static final Migration migration_1_2 = new Migration(1, 2) {
+
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE PhoneCountryCode ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+                    + "geo_code TEXT,"
+                    + "country_code TEXT);");
+            PhoneCountryCode[] countryCodes = PhoneCountryCode.populateSeedData();
+            for (PhoneCountryCode item : countryCodes) {
+                database.execSQL(String.format("INSERT INTO PhoneCountryCode (geo_code, country_code) VALUES ('%1$s', '%2$s');",
+                        item.getGeoCode(), item.getCountryCode()));
+            }
+        }
+    };
+
     private static AppDatabase buildDatabase(final Context context) {
-        return Room.databaseBuilder(context, AppDatabase.class, "shakticoin")
+        return Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "shakticoin")
                 .addCallback(new Callback() {
                     @Override
                     public void onCreate(@NonNull SupportSQLiteDatabase db) {
@@ -35,11 +60,14 @@ public abstract class AppDatabase extends RoomDatabase {
                         Executors.newSingleThreadExecutor().execute(new Runnable() {
                             @Override
                             public void run() {
-                                getDatabase(context).geoDao().insertAll(Geo.populateSeedData(context));
+                                AppDatabase db = getDatabase(context);
+                                db.geoDao().insertAll(Geo.populateSeedData(context));
+                                db.phoneCountryCodeDao().insertAll(PhoneCountryCode.populateSeedData());
                             }
                         });
                     }
                 })
+                .addMigrations(migration_1_2)
                 .build();
     }
 }
