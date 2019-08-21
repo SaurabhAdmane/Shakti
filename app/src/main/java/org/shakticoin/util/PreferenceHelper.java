fc0e8a774f2a9f79a07f80e3d41c975e5f9e45b7
Oleg Andreyev <andreyev.oleg@gmail.com>
2019-08-21 22:56:23 +0300

    Clean up #SHAK-54

--- app/src/main/java/org/shakticoin/util/PreferenceHelper.java
+++ app/src/main/java/org/shakticoin/util/PreferenceHelper.java
@@ -1,8 +1,5 @@
 package org.shakticoin.util;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
 public class PreferenceHelper {
     public static String GENERAL_PREFERENCES = "org.shakticoin.GENERAL";
 
@@ -12,6 +9,6 @@ public class PreferenceHelper {
     /** Boolean preference that is true if user has been logged in at least once */
     public static String PREF_KEY_HAS_ACCOUNT = "hasAccount";
 
-    /** Authorization tocken for automatic login */
+    /** Authorization token for automatic login */
     public static String PREF_KEY_TOKEN = "t";
 }
