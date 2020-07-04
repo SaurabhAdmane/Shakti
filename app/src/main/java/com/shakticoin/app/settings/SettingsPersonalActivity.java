bee161b0967f4f503888d53f8e293ca6b5ae4da7
Oleg Andreyev <andreyev.oleg@gmail.com>
2020-07-04 10:22:55 +0300

    Complete personal information

--- app/src/main/java/com/shakticoin/app/settings/SettingsPersonalActivity.java
+++ app/src/main/java/com/shakticoin/app/settings/SettingsPersonalActivity.java
@@ -2,6 +2,7 @@ package com.shakticoin.app.settings;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,8 +15,8 @@ import com.shakticoin.app.api.OnCompleteListener;
 import com.shakticoin.app.api.Session;
 import com.shakticoin.app.api.UnauthorizedException;
 import com.shakticoin.app.api.kyc.KYCRepository;
-import com.shakticoin.app.api.user.UserRepository;
 import com.shakticoin.app.databinding.ActivityPersonalInfoBinding;
+import com.shakticoin.app.util.CommonUtil;
 import com.shakticoin.app.util.Debug;
 import com.shakticoin.app.widget.DrawerActivity;
 
@@ -24,7 +25,6 @@ import java.util.Map;
 public class SettingsPersonalActivity extends DrawerActivity {
     private ActivityPersonalInfoBinding binding;
     private SettingsPersonalViewModel viewModel;
-    private UserRepository userRepo;
     private KYCRepository kycRepository = new KYCRepository();
 
     @Override
@@ -36,20 +36,23 @@ public class SettingsPersonalActivity extends DrawerActivity {
         binding.setViewModel(viewModel);
 
         onInitView(binding.getRoot(), getString(R.string.settings_personal_title), true);
+    }
 
-        userRepo = new UserRepository();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        final Activity activity = this;
         binding.progressBar.setVisibility(View.VISIBLE);
-        Activity self = this;
         kycRepository.getUserDetails(new OnCompleteListener<Map<String, Object>>() {
             @Override
             public void onComplete(Map<String, Object> values, Throwable error) {
                 binding.progressBar.setVisibility(View.INVISIBLE);
                 if (error != null) {
                     if (error instanceof UnauthorizedException) {
-                        startActivity(Session.unauthorizedIntent(self));
+                        startActivity(Session.unauthorizedIntent(activity));
                     } else {
-                        Toast.makeText(self, Debug.getFailureMsg(self, error), Toast.LENGTH_LONG).show();
+                        Toast.makeText(activity, Debug.getFailureMsg(activity, error), Toast.LENGTH_LONG).show();
                     }
                     return;
                 }
@@ -57,6 +60,26 @@ public class SettingsPersonalActivity extends DrawerActivity {
                 viewModel.firstName.setValue((String) values.get("firstName"));
                 viewModel.middleName.setValue((String) values.get("middleName"));
                 viewModel.lastName.setValue((String) values.get("lastName"));
+                viewModel.birthDate.setValue((String) values.get("dob"));
+                String emailAddress = (String) values.get("primaryEmail");
+                if (TextUtils.isEmpty(emailAddress)) emailAddress = (String) values.get("secondaryEmail");
+                viewModel.emailAddress.setValue(emailAddress);
+                String phoneNumber = (String) values.get("primaryMobile");
+                if (TextUtils.isEmpty(phoneNumber)) phoneNumber = (String) values.get("secondaryMobile");
+                viewModel.phoneNumber.setValue(phoneNumber);
+                viewModel.occupation.setValue((String) values.get("occupation"));
+                viewModel.education.setValue((String) values.get("education1"));
+
+                Map<String, String> address = CommonUtil.checkMap(values.get("address"));
+                if (address != null && address.size() > 0) {
+                    viewModel.address1.setValue(address.get("address1"));
+                    viewModel.address2.setValue(address.get("address2"));
+                    viewModel.city.setValue(address.get("city"));
+                    viewModel.stateProvince.setValue(address.get("stateProvinceCode"));
+                    viewModel.country.setValue(address.get("country"));
+                    viewModel.postalCode.setValue(address.get("zipCode"));
+                }
+
             }
         });
     }
--- app/src/main/java/com/shakticoin/app/settings/SettingsPersonalViewModel.java
+++ app/src/main/java/com/shakticoin/app/settings/SettingsPersonalViewModel.java
@@ -8,5 +8,18 @@ public class SettingsPersonalViewModel extends ViewModel {
     public MutableLiveData<String> firstName = new MutableLiveData<>();
     public MutableLiveData<String> middleName = new MutableLiveData<>();
     public MutableLiveData<String> lastName = new MutableLiveData<>();
+    public MutableLiveData<String> birthDate = new MutableLiveData<>();
+    public MutableLiveData<String> occupation = new MutableLiveData<>();
+    public MutableLiveData<String> education = new MutableLiveData<>();
+
+    public MutableLiveData<String> emailAddress = new MutableLiveData<>();
+    public MutableLiveData<String> phoneNumber = new MutableLiveData<>();
+
+    public MutableLiveData<String> address1 = new MutableLiveData<>();
+    public MutableLiveData<String> address2 = new MutableLiveData<>();
+    public MutableLiveData<String> city = new MutableLiveData<>();
+    public MutableLiveData<String> stateProvince = new MutableLiveData<>();
+    public MutableLiveData<String> postalCode = new MutableLiveData<>();
+    public MutableLiveData<String> country = new MutableLiveData<>();
 
 }
