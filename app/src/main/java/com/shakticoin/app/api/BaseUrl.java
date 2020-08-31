897dd371e9dc6356f56f8eeaa0ee669c4cb9e227
Oleg Andreyev <andreyev.oleg@gmail.com>
2020-08-31 16:48:20 +0300

    Change URLs

--- app/src/main/java/com/shakticoin/app/api/BaseUrl.java
+++ app/src/main/java/com/shakticoin/app/api/BaseUrl.java
@@ -6,10 +6,10 @@ package com.shakticoin.app.api;
 public class BaseUrl {
     public static final String BASE_URL = "https://dev-api.shakticoin.com/";
     public static final String IAM_BASE_URL = "https://iam-stg.shakticoin.com/oxauth/restv1/";
-    public static final String WALLETSERVICE_BASE_URL = "https://walletservice-stg.shakticoin.com/walletservice/api/v1/";
-    public static final String LICENSESERVICE_BASE_URL = "https://licenseservice-stg.shakticoin.com/license-service/api/v1/";
-    public static final String KYC_USER_SERVICE_BASE_URL = "https://kycuser-stg.shakticoin.com/kyc-user-service/api/v1/";
-    public static final String KYC_CORP_SERVICE_BASE_URL = "https://kyccorpservice-stg.shakticoin.com/???";
+    public static final String WALLETSERVICE_BASE_URL = "https://walletservice-qa.shakticoin.com/walletservice/api/v1/";
+    public static final String LICENSESERVICE_BASE_URL = "https://licenseservice-qa.shakticoin.com/license-service/api/v1/";
+    public static final String KYC_USER_SERVICE_BASE_URL = "http://kycuser-qa.shakticoin.com/kyc-user-service/api/v1/";
+    public static final String KYC_CORP_SERVICE_BASE_URL = "https://kyccorpservice-qa.shakticoin.com/???";
     public static final String PHONE_OTP_SERVICE_BASE_URL = "https://mobileotpservice-qa.shakticoin.com/sms-otp-service/api/v1/";
     public static final String EMAIL_OTP_SERVICE_BASE_URL = "https://emailotpservice-qa.shakticoin.com/email-otp-service/api/v1/";
     public static final String ONBOARD_SERVICE_BASE_URL = "https://onboardshakti-qa.shakticoin.com/onboardshakti-service/api/v1/";
