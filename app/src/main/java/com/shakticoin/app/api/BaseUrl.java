855d5e3352054830e4305ee7a4df91726ba5bb68
Oleg Andreyev <andreyev.oleg@gmail.com>
2020-09-10 12:59:09 +0300

    Update IAM QA URL

--- app/src/main/java/com/shakticoin/app/api/BaseUrl.java
+++ app/src/main/java/com/shakticoin/app/api/BaseUrl.java
@@ -5,7 +5,7 @@ package com.shakticoin.app.api;
  */
 public class BaseUrl {
     public static final String BASE_URL = "https://dev-api.shakticoin.com/";
-    public static final String IAM_BASE_URL = "https://iam-stg.shakticoin.com/oxauth/restv1/";
+    public static final String IAM_BASE_URL = "https://iam-qa.shakticoin.com/oxauth/restv1/";
     public static final String WALLETSERVICE_BASE_URL = "https://walletservice-qa.shakticoin.com/walletservice/api/v1/";
     public static final String LICENSESERVICE_BASE_URL = "https://licenseservice-qa.shakticoin.com/license-service/api/v1/";
     public static final String KYC_USER_SERVICE_BASE_URL = "http://kycuser-qa.shakticoin.com/kyc-user-service/api/v1/";
