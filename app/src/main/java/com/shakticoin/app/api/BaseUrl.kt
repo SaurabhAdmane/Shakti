42ea9242ffc4d068fd81f3b242bc96f39e9fddbb
Oleg Andreyev <andreyev.oleg@gmail.com>
2020-09-11 14:32:38 +0300

    Use secure connection to KYC service

--- app/src/main/java/com/shakticoin/app/api/BaseUrl.kt
+++ app/src/main/java/com/shakticoin/app/api/BaseUrl.kt
@@ -11,7 +11,7 @@ object BaseUrl {
     const val IAM_BASE_URL = "https://iam${ENV_CODE}.shakticoin.com/oxauth/restv1/"
     const val WALLETSERVICE_BASE_URL = "https://walletservice${ENV_CODE}.shakticoin.com/walletservice/api/v1/"
     const val LICENSESERVICE_BASE_URL = "https://licenseservice${ENV_CODE}.shakticoin.com/license-service/api/v1/"
-    const val KYC_USER_SERVICE_BASE_URL = "http://kycuser${ENV_CODE}.shakticoin.com/kyc-user-service/api/v1/"
+    const val KYC_USER_SERVICE_BASE_URL = "https://kycuser${ENV_CODE}.shakticoin.com/kyc-user-service/api/v1/"
     const val KYC_CORP_SERVICE_BASE_URL = "https://kyccorpservice${ENV_CODE}.shakticoin.com/???"
     const val PHONE_OTP_SERVICE_BASE_URL = "https://mobileotpservice${ENV_CODE}.shakticoin.com/sms-otp-service/api/v1/"
     const val EMAIL_OTP_SERVICE_BASE_URL = "https://emailotpservice${ENV_CODE}.shakticoin.com/email-otp-service/api/v1/"
